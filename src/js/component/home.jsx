7bf1985f04b8e597f3a1b4f1f6d255c81c51f7ed
Robert <Robert1cabrera@gmail.com>
2023-07-01 01:08:21 +0000

    almost

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,44 +4,53 @@ import React, { useState } from "react";
 
 
 const Home = () => {
-const [userInput, setUserInput] = useState(""); 
-const [todos, setTodos] = useState([]);
-	
-return (
-	<div className="wrapper">
-		<h1>Todos</h1>
-		<ul>
-			<li>
-				<input type="text"
-                value={userInput}
-                onChange={(e) => setUserInput(e.target.value)}
-                onKeyUp={(e) => {
-					if (e.key === "Enter") {
-						setTodos(todos.concat([userInput]));
-						setUserInput("");
-					}
-				}}
-				placeholder="what do you need to do?"></input>
-			</li>
-			{todos.map((item, index) => (
-				<li>
-					{item}{" "}
-					<i 
-						class="fas fa-trash-alt"
-						onClick={() => 
-							setTodos(
-								todos.filter(
-									(t, currentIndex) => 
-										index != currentIndex
-								)
-							)
-						}></i>
-				</li>
-			))}
-		</ul>	
-		<div>{todos.length} tasks</div>	
+	const [userInput, setUserInput] = useState("");
+	const [todos, setTodos] = useState([]);
 
-			
+	return (
+		<div className="wrapper">
+			<h1>Todos</h1>
+			<div className="todo-body">
+				<ul className="todo-list">
+					{/* <div className="todo-list"> */}
+					<li>
+						<input type="text"
+							value={userInput}
+							onChange={(e) => setUserInput(e.target.value)}
+							onKeyUp={(e) => {
+								if (e.key === "Enter") {
+									setTodos(todos.concat([userInput]));
+									setUserInput("");
+								}
+							}}
+							placeholder="what do you need to do?"></input>
+					</li>
+					{todos.map((item, index) => (
+						<li className="todo-list-item">
+							{/* <span class="todo-item-text"> */}
+							{item}{" "}
+							{/* </span> */}
+							{/* <span className="todo-item-icon"> */}
+							<i
+								class="fas fa-trash-alt"
+								onClick={() =>
+									setTodos(
+										todos.filter(
+											(t, currentIndex) =>
+												index != currentIndex
+										)
+									)
+								}></i>
+							{/* </span> */}
+						</li>
+
+					))}
+					{/* </div> */}
+				</ul>
+			</div>
+			<div>{todos.length} items left</div>
+
+
 		</div>
 	);
 };
